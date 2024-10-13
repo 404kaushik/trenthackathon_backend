ba46b42e10aac638c8a6965b3ec00c06751c789f
Kaushik Nag Tumu <138563218+404kaushik@users.noreply.github.com>
2024-10-13 03:23:34 -0400

    using appropriate addresses

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'www.hacktrent.ca', // or '*'
+  origin: ['https://trenthackathon.vercel.app/', 'https://hacktrent.ca/' ], // or '*'
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed request headers
   credentials: true,
 }));
 
